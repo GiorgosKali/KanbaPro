961b1441dc7995e4d06f2573ab4d0fd90c4de157
daoik <dimeconomu@gmail.com>
2023-10-15 02:51:09 +0300

    [webApp] small visual fixes

--- webApp/src/pages/components/list/List.tsx
+++ webApp/src/pages/components/list/List.tsx
@@ -8,7 +8,7 @@ const List = (list: tList) => {
     const [showAddButton, setShowAddButton] = useState(false);
     return (
         <>
-            <div className="p-3 w-96 h-max rounded-lg space-y-4 shadow bg-gray-900 text-slate-400 border border-[#101204] hover:border-slate-700 transition-all duration-50">
+            <div className="p-3 w-96 h-max min-h-[7rem] rounded-lg space-y-4 shadow bg-gray-900 text-slate-400 border border-[#101204] hover:border-slate-700 transition-all duration-50">
                 <div className="inline-flex w-full justify-between">
                     {/* Title */}
                     <textarea className="text-xl me-1 w-full font-semibold p-1 h-10 bg-inherit hover:bg-gray-800 focus:bg-[#242424] focus:text-gray-50 resize-none rounded-lg  focus:outline outline-1 outline-[#646cff] "
@@ -27,7 +27,7 @@ const List = (list: tList) => {
                 <div className="h-5 hover:h-10 transition-all duration-200 footer text-slate-400  w-full flex justify-center" onMouseOver={() => setShowAddButton(true)} onMouseLeave={() => setShowAddButton(false)}>
 
 
-                    {showAddButton && <button onClick={() => store.addNewCardtoList(list.id)} className="text-lg rounded-[50%]  hover:text-white flex self-center bg-gray-900 hover:bg-[#242424] font-semibold p-2 outline-1 active:outline outline-[#646cff] ">
+                    {showAddButton && <button onClick={() => store.addNewCardtoList(list.id)} className="text-lg rounded-[50%]  hover:text-white flex self-center bg-gray-900 hover:bg-gray-700 active:bg-[#242424] font-semibold p-2 outline-1 active:outline outline-[#646cff] ">
                         <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                         </svg>
