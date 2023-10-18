d6505e23d4b0979de6c45c6cec3a66405308640f
Giorgos <giorgos.k.kalligiannakis@gmail.com>
2023-10-18 22:14:34 +0300

    [webApp] api client generated

--- webApp/src/App.tsx
+++ webApp/src/App.tsx
@@ -1,12 +1,25 @@
 import Home from './pages/home/Home'
 import './App.css'
 
-function App() {
+import * as kanba from './apis/kanba';
 
+function App() {
+  const testApi = async () => {
+
+    const userApi = kanba.UsersApiFactory({
+      basePath:'http://localhost:8000',
+      isJsonMime:()=>true,
+    })
+    const authApi = kanba.AuthApiFactory({
+      basePath:'http://localhost:8000',
+      isJsonMime:()=>true,
+    })
+
+  }
 
   return (
     <>
-    
+      <button className="btn btn-primary" onClick={()=>testApi()}>Click me</button>
      <Home />
     </>
   )
